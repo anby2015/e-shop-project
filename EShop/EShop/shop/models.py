2e0ec6c24b401c3cf771cc789880bdaf4f6d5e8e
Dekart <Dekart@Dekart-??.hostel16.univ>
2012-04-13 03:06:40 +0300

    ??????? ???????

--- EShop/EShop/shop/models.py
+++ EShop/EShop/shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.forms import User
-
+from django.utils.translation import ugettext
 
 class UserInfo(models.Model):
     name = models.CharField(max_length=30)
@@ -26,15 +26,24 @@ class CompanyProfile(UserInfo):
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = ugettext("Category")
+        verbose_name_plural = ugettext("Categories")
     name = models.CharField(max_length=50)
 
 
 class SubCategory(models.Model):
+    class Meta:
+        verbose_name = ugettext("Subcategory")
+        verbose_name_plural = ugettext("Subcategories")
     name = models.CharField(max_length=50)
     category = models.ForeignKey(Category)
 
 
 class Product(models.Model):
+    class Meta:
+        verbose_name = ugettext("Product")
+        verbose_name_plural = ugettext("Products")
     name = models.CharField(max_length=100)
     sub_category = models.ForeignKey(SubCategory)
     seller = models.ForeignKey(User)
@@ -44,17 +53,26 @@ class Product(models.Model):
 
 
 class WebFormularMessage(models.Model):
+    class Meta:
+        verbose_name = ugettext("Message")
+        verbose_name_plural = ugettext("Messages")
     user = models.ForeignKey(User, null=True, blank=True)
     message = models.CharField(max_length=2000)
     email = models.EmailField()
 
 
 class Banner(models.Model):
+    class Meta:
+        verbose_name = ugettext("Banner")
+        verbose_name_plural = ugettext("Banners")
     url = models.URLField()
     picture = models.ImageField(upload_to='/tmp')
 
 
 class News(models.Model):
+    class Meta:
+        verbose_name = ugettext("News")
+        verbose_name_plural = ugettext("News_p")
     date = models.DateTimeField()
     title = models.CharField(max_length=300)
     text = models.CharField(max_length=3000)
